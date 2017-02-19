94ebfd4b98e882965b51087bb02d79a6b24a343f
Yann Zaoui <yzaoui7@gmail.com>
2017-02-19 12:39:59 -0500

    Add initial version of program

---
+++ Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View(model);
+    }
+}
---
+++ Model.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class Model {
+    /** The observers that are watching this model for changes. */
+    private List<Observer> observers;
+
+    /**
+     * Create a new model.
+     */
+    public Model() {
+        this.observers = new ArrayList();
+    }
+
+    /**
+     * Add an observer to be notified when this model changes.
+     */
+    public void addObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    /**
+     * Remove an observer from this model.
+     */
+    public void removeObserver(Observer observer) {
+        this.observers.remove(observer);
+    }
+
+    /**
+     * Notify all observers that the model has changed.
+     */
+    public void notifyObservers() {
+        for (Observer observer: this.observers) {
+            observer.update(this);
+        }
+    }
+}
